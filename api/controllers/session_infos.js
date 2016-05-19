3dbb1439a2eef5a16787dfd101e0423b8050469e
Nikola Nesovic <svetskigigacar@Nikolas-MacBook-Pro.local>
2016-05-19 14:43:41 +0200

    added session info latency

--- api/controllers/session_infos.js
+++ api/controllers/session_infos.js
@@ -80,14 +80,18 @@ function putSessionInfoItem(req, res) {
     }
   };
 
-  awsdb.put(params, function(err, data) {
+  function putData () {
+    awsdb.put(params, function(err, data) {
     if (err) {
       console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
       res.json("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      putData();
     } else {
       console.log("Added item:", JSON.stringify(data, null, 2));
       res.json(data);
     }
   });
+  }
+  putData();
   res.statusCode = 201;
 }
