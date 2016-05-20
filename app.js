2a06b38de2088d0604b9bd8b0aed9e46a042b547
Nikola Nesovic <svetskigigacar@Nikolas-MacBook-Pro.local>
2016-05-20 16:59:45 +0200

    app javascript

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ a127.init(function(config) {
     }
 
     // Return a JSON representation of #/definitions/ErrorResponse
-    res.set('Content-Type', 'application/json');
+    res.set('Content-Type', 'application/javascript');
     res.end(JSON.stringify(err));
   });
 
