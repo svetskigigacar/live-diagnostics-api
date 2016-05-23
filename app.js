788a76d6038c5da86ba855a461e38817154dd48c
Nikola Nesovic <svetskigigacar@Nikolas-MacBook-Pro.local>
2016-05-23 13:50:33 +0200

    added options

--- app.js
+++ app.js
@@ -28,6 +28,18 @@ a127.init(function(config) {
     }
 
     // Return a JSON representation of #/definitions/ErrorResponse
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+    
     res.set('Content-Type', 'text/javascript');
     res.end(JSON.stringify(err));
   });
